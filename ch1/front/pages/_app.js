ee59b9162350b603d514010a2ae9f19c0c11e24a
moonhopark <12171928@inha.edu>
2021-01-27 19:35:22 +0900

    반응형 그리드로 상단 메뉴 만들기, 로그인 폼 만들기

--- ch1/front/pages/_app.js
+++ ch1/front/pages/_app.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Head from 'next/head';
 import 'antd/dist/antd.css'
 
 const NodeBird = ({ Component }) => {
   return (
     <>
+      <Head>
+        <meta charSet="utf-8" />
+        <title>NodeBird</title>
+      </Head>
       <div>공통 메뉴</div>
       <Component />
     </>
