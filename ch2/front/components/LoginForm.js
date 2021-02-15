f2323d8f0169a0e4cafb992abc74ab1105469357
moonhopark <moonho4199@naver.com>
2021-02-15 17:50:33 +0900

    redux로 데 변경하는 법 구현

--- ch2/front/components/LoginForm.js
+++ ch2/front/components/LoginForm.js
@@ -1,10 +1,11 @@
-import React, { useState, useCallback } from 'react';
+import React, { useCallback } from 'react';
 import { Button, Form, Input } from 'antd';
 import Link from 'next/link';
 import styled from 'styled-components';
-import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
 
 import useInput from '../hooks/useInput';
+import { loginAction } from '../reducers'
 
 const ButtonWrapper = styled.div`
   margin-top: 10px;
@@ -15,11 +16,13 @@ const FormWrapper = styled(Form)`
 `;
 
 const LoginForm = ({ setIsLoggedIn }) => {
+  const dispatch = useDispatch();
   const [id, onChangeId] = useInput('');
   const [password, onChangePassword] = useInput('')
 
   const onSubmitForm = useCallback(() => {
     console.log(id, password);
+    dispatch(loginAction({ id, password }));
     setIsLoggedIn(true);
   },[id, password]);
   
@@ -48,9 +51,5 @@ const LoginForm = ({ setIsLoggedIn }) => {
     </FormWrapper>
   );
 }
-
-LoginForm.propTypes = {
-  setIsLoggedIn: PropTypes.func.isRequired,
-};
 
 export default LoginForm;
--- ch2/front/components/UserProfile.js
+++ ch2/front/components/UserProfile.js
@@ -1,10 +1,15 @@
 import React, { useCallback } from 'react';
+import { useDispatch } from 'react-redux';
 import { Card, Avatar, Button } from 'antd';
 import PropTypes from 'prop-types';
 
-const UserProfile = ({ setIsLoggedIn }) => {
+import { logoutAction } from '../reducers'
+
+const UserProfile = () => {
+  const dispatch = useDispatch();
+
   const onLogOut = useCallback(() => {
-    setIsLoggedIn(false);
+    dispatch(logoutAction());
   }, []);
 
   return (
