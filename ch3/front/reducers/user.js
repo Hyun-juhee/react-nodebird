76ff1466d4dfddf45d610bda452ac5bff6a035ad
moonhopark <moonho4199@naver.com>
2021-02-22 17:48:11 +0900

    redux-thunk 써보기

--- ch3/front/reducers/user.js
+++ ch3/front/reducers/user.js
@@ -6,15 +6,35 @@ export const initialState = {
 }
 
 export const loginAction = (data) => {
+  return (dispatch, getState) => {
+    const state = getState();
+    dispatch(loginRequestAction());
+    axois.post('/api/login')
+      .then((res) => {
+        dispatch(loginSuccessAction(res.data));
+      })
+      .catch((err) => {
+        dispatch(loginFailureAction(err));
+      })
+  }
+}
+
+export const loginRequestAction = (data) => {
   return {
-    type: 'LOG_IN',
+    type: 'LOG_IN_REQUEST',
     data,
   }
 }
 
-export const logoutAction = () => {
+export const loginSuccessAction = () => {
   return {
-    type: 'LOG_OUT',
+    type: 'LOG_IN_SUCCESS',
+  }
+}
+
+export const loginFailureAction = () => {
+  return {
+    type: 'LOG_IN_FAILURE',
   }
 }
 
